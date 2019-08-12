abfe45c66b2801c9189f460bdc04ff86e20b9493
mini-ninja-64 <callumhart@btinternet.com>
2019-08-12 15:02:20 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(input)
+    @word = input
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @word.split("").sort}
+  end   
+
+end 
+
+
