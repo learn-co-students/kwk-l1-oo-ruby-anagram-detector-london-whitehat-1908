7631e3178c0214cd54fb525c01b3b427ddf0f487
jack272hughes <jack272hughes@gmail.com>
2019-08-13 14:15:31 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    def initialize(string)
+        @string_to_find = string
+    end
+
+    def match(anagrams)
+        string_letters = @string_to_find.split("").sort
+        buffer = []
+        anagrams.each {|n|
+            anagram_letters = n.split("").sort
+            if anagram_letters == string_letters
+                buffer << n
+            end
+        }
+        return buffer
+    end
+end
